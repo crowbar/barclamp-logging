7093e6977dd5ce8aa62a9b4f1e3c0e3a7d179a6e
Vincent Untz <vuntz@suse.com>
2015-09-09 11:29:12 +0200

    Add note that this got moved to merged repo

--- chef/cookbooks/logging/metadata.rb
+++
@@ -1,8 +0,0 @@
-maintainer       "Dell Crowbar Team"
-maintainer_email "openstack@dell.com"
-license          "Apache 2.0"
-description      "Installs logging components for Crowbar"
-long_description "Installs logging components for Crowbar"
-version          "1.0.0"
-depends "utils"
-
